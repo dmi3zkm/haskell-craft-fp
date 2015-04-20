a424aafd14d03a44b660ee28ad6148767787727c
dmi3 <dmitriy.zakomirnyy@gmail.com>
2015-04-20 19:17:10 +0300

    Implement exercises 3.14 and 3.15

---
+++ ch03/cond.hs
@@ -0,0 +1,14 @@
+import Test.QuickCheck
+
+--exercise 3.14
+myMin :: Int -> Int -> Int
+myMin x y 
+    | x < y = x
+    | otherwise = y
+
+minThree :: Int -> Int -> Int -> Int
+minThree x y z = myMin (min x y) z
+
+--exercise 3.15
+prop_compareMin x y = min x y == myMin x y
+prop_minThree x y z =  min (min x y) z == minThree x y z
