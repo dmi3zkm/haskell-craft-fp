06f9a8fd71f49fe2ef720f143f5589813d366590
dmi3 <dmitriy.zakomirnyy@gmail.com>
2015-04-20 23:40:02 +0300

    Add function type in exercise 3.15

--- ch03/cond.hs
+++ ch03/cond.hs
@@ -10,5 +10,8 @@ minThree :: Int -> Int -> Int -> Int
 minThree x y z = myMin (min x y) z
 
 --exercise 3.15
+prop_compareMin :: Int -> Int -> Bool
 prop_compareMin x y = min x y == myMin x y
+
+prop_minThree :: Int -> Int -> Int -> Bool
 prop_minThree x y z =  min (min x y) z == minThree x y z
