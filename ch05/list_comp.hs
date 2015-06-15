9ba3792b89f8182f76eca3401538268b49b8f30a
dmi3 <dmitriy.zakomirnyy@gmail.com>
2015-06-15 16:02:05 +0300

    Implement exercise 5.19

--- ch05/list_comp.hs
+++ ch05/list_comp.hs
@@ -1,2 +1,10 @@
+import Data.Char
+
 doubleAll :: [Integer] -> [Integer]
 doubleAll xs = [ x*2 | x <- xs]
+
+capitalize :: String -> String
+capitalize xs = [ toUpper x | x <- xs]
+
+capitalizeLetters :: String -> String
+capitalizeLetters xs = [ toUpper x | x <- xs, isLetter x]
