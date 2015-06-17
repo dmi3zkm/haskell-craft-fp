e858ea81d725e85736c14827e383cd70e588a217
dmi3 <dmitriy.zakomirnyy@gmail.com>
2015-06-17 22:23:07 +0300

    Implement exercise 6.7

--- ch06/Picture.hs
+++ ch06/Picture.hs
@@ -32,3 +32,13 @@ superimposeLine str1 str2 = [ superimposeChar ch1 ch2 | (ch1, ch2) <- zip str1 s
 --exercise 6.6
 superimpose :: Picture -> Picture -> Picture
 superimpose pic1 pic2 = [ superimposeLine line1 line2 | (line1, line2) <- zip pic1 pic2 ]
+
+--exercise 6.7
+joinStrings :: [String] -> String
+joinStrings ss = [ c | s <- ss, c <- s ]
+
+onSeparateLines :: [String] -> [String]
+onSeparateLines ss = [s ++ "\n" | s <- ss]
+
+printPicture :: Picture -> IO ()
+printPicture pic = putStr $ joinStrings $ onSeparateLines pic
