fb1e5cf98a090d2471f24f12408e4e0539cf2358
dmi3 <dmitriy.zakomirnyy@gmail.com>
2015-06-17 18:15:08 +0300

    Implement Picture example

---
+++ ch06/Picture.hs
@@ -0,0 +1,22 @@
+type Picture = [[Char]]
+
+flipH :: Picture -> Picture
+flipH = reverse
+
+above :: Picture -> Picture -> Picture
+above = (++)
+
+flipV :: Picture -> Picture
+flipV pic = [reverse line | line <- pic]
+
+beside :: Picture -> Picture -> Picture
+beside picL picR = [ lineL ++ lineR | (lineL, lineR) <- zip picL picR ]
+
+invertChar :: Char -> Char 
+invertChar ch = if ch == '.' then '#' else '.'
+
+invertLine :: [Char] -> [Char]
+invertLine line = [ invertChar ch | ch <- line ]
+
+invertColour :: Picture -> Picture
+invertColour pic = [ invertLine line | line <- pic ]
