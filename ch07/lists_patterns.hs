725e51c4f91f3b559261f9e957cafb67775c2714
dmi3 <dmitriy.zakomirnyy@gmail.com>
2015-06-25 23:05:14 +0300

    Implement exercise 7.3

--- ch07/lists_patterns.hs
+++ ch07/lists_patterns.hs
@@ -8,3 +8,10 @@ sumFirstAndSecond :: [Integer] -> Integer
 sumFirstAndSecond [] = 0
 sumFirstAndSecond (x:[]) = x
 sumFirstAndSecond (x1:(x2:xs)) = x1 + x2
+
+--exercise 7.3
+firstPlusOne' :: [Integer] -> Integer
+firstPlusOne' xs = if null xs then 0 else head xs + 1
+
+sumFirstAndSecond' :: [Integer] -> Integer
+sumFirstAndSecond' xs = if null xs then 0 else (if null (tail xs) then head xs else head xs + head (tail xs))
