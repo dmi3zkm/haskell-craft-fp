a520a8d91b5b021b3d5685a556cd332566a604bd
dmi3 <dmitriy.zakomirnyy@gmail.com>
2015-06-25 23:39:34 +0300

    Implement exercise 7.4

--- ch07/lists_patterns.hs
+++ ch07/lists_patterns.hs
@@ -1,3 +1,5 @@
+import Data.Char (isDigit)
+
 --exercise 7.1
 firstPlusOne :: [Integer] -> Integer
 firstPlusOne [] = 0
@@ -15,3 +17,10 @@ firstPlusOne' xs = if null xs then 0 else head xs + 1
 
 sumFirstAndSecond' :: [Integer] -> Integer
 sumFirstAndSecond' xs = if null xs then 0 else (if null (tail xs) then head xs else head xs + head (tail xs))
+
+--exercise 7.4
+firstDigit :: String -> Char
+firstDigit xs
+    | null xs           = '\0'
+    | isDigit $ head xs = head xs
+    | otherwise         = '\0'
