4099cf0a464a923e4d56889fbf94db040e78e825
dmi3 <dmitriy.zakomirnyy@gmail.com>
2015-06-25 22:53:22 +0300

    Implement exercise 7.2

--- ch07/lists_patterns.hs
+++ ch07/lists_patterns.hs
@@ -2,3 +2,9 @@
 firstPlusOne :: [Integer] -> Integer
 firstPlusOne [] = 0
 firstPlusOne (x:xs) = x + 1
+
+--exercise 7.2
+sumFirstAndSecond :: [Integer] -> Integer
+sumFirstAndSecond [] = 0
+sumFirstAndSecond (x:[]) = x
+sumFirstAndSecond (x1:(x2:xs)) = x1 + x2
