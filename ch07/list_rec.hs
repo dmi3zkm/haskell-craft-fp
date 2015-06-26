1ef06fd8f199477ff095be6fccab08440ac01b22
dmi3 <dmitriy.zakomirnyy@gmail.com>
2015-06-26 22:46:38 +0300

    Implement exercise 7.6

--- ch07/list_rec.hs
+++ ch07/list_rec.hs
@@ -1,6 +1,15 @@
-import Prelude hiding (product)
+import Prelude hiding (product, and, or)
 
 --exercise 7.5
 product :: [Integer] -> Integer
 product [] = 1
 product (x:xs) = x * product xs
+
+--exercis 7.6
+and, or :: [Bool] -> Bool
+
+and [] = True
+and (x:xs) = x && (and xs)
+
+or [] = False
+or (x:xs) = x || (or xs)
